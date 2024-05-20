edd2966dfa37cd54ea448d65081c1ca4f9508000
Alejo Mancinelli <alejomancinelli98@gmail.com>
2024-05-20 17:02:24 -0300

    Cambios en la App

--- code/AndriodApp/JONICA/app/src/main/java/com/dalle_boca/jonica/HomeFragment.java
+++ code/AndriodApp/JONICA/app/src/main/java/com/dalle_boca/jonica/HomeFragment.java
@@ -2,17 +2,59 @@ package com.dalle_boca.jonica;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class HomeFragment extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+import android.widget.ImageButton;
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+public class HomeFragment extends Fragment {
+    private ImageButton buttonPlay;
+    private ImageButton buttonPause;
+    private ImageButton buttonStop;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        buttonPlay = rootView.findViewById(R.id.button_play);
+        buttonPause = rootView.findViewById(R.id.button_pause);
+        buttonStop = rootView.findViewById(R.id.button_stop);
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPlay.setImageResource(R.drawable.ic_play_button_pressed);
+                buttonPause.setImageResource(R.drawable.ic_pause_button);
+                buttonStop.setImageResource(R.drawable.ic_stop_button);
+
+                // TODO: Publicar en MQTT que diga "play"
+            }
+        });
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPlay.setImageResource(R.drawable.ic_play_button);
+                buttonPause.setImageResource(R.drawable.ic_pause_button_pressed);
+                buttonStop.setImageResource(R.drawable.ic_stop_button);
+
+                // TODO: Publicar en MQTT que diga "pause"
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPlay.setImageResource(R.drawable.ic_play_button);
+                buttonPause.setImageResource(R.drawable.ic_pause_button);
+                buttonStop.setImageResource(R.drawable.ic_stop_button_pressed);
+
+                // TODO: Publicar en MQTT que diga "stop"
+            }
+        });
+
+        return rootView;
     }
 }
